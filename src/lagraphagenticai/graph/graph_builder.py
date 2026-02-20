ace95c68ec3c0e1ae005a44bb13f74e05cc55932
Kabeer <Kabeer.nagpal@outlook.com>
2026-02-20 23:02:44 +0530

    Integrated Tavily API for web search capability.

--- src/lagraphagenticai/graph/graph_builder.py
+++ src/lagraphagenticai/graph/graph_builder.py
@@ -2,6 +2,9 @@ from langgraph.graph import StateGraph
 from src.lagraphagenticai.state.state import State
 from langgraph.graph import START, END
 from src.lagraphagenticai.nodes.basic_chatbot_node import BasicChatbotNode
+from src.lagraphagenticai.tools.search_tool import get_tools, create_tool_node
+from langgraph.prebuilt import tools_condition, ToolNode
+from src.lagraphagenticai.nodes.chatbot_with_tool_node import ChatbotWithToolNode
 
 class GraphBuilder:
     def __init__(self,model):
@@ -22,6 +25,30 @@ class GraphBuilder:
         self.graph_builder.add_edge(START, "chatbot")
         self.graph_builder.add_edge("chatbot",END)
 
+    def chatbot_with_tools_build_graph(self):
+        """
+        Building Chatbot with Tools here.
+        """
+        ##Define the tool and tool node
+        tools = get_tools()
+        tool_node = create_tool_node(tools)
+
+        #Define the LLM
+        llm=self.llm
+
+        #Define the chatbot node
+        obj_chatbot_with_node = ChatbotWithToolNode(llm)
+        chatbot_node = obj_chatbot_with_node.create_chatbot(tools)
+        
+        #Add nodes
+        self.graph_builder.add_node("chatbot",chatbot_node)
+        self.graph_builder.add_node("tools", tool_node)
+        #Define Conditional and direct edges
+        self.graph_builder.add_edge(START, "chatbot")
+        self.graph_builder.add_conditional_edges("chatbot", tools_condition)
+        self.graph_builder.add_edge("tools", "chatbot")
+        # self.graph_builder.add_edge("chatbot", END)
+
     def setup_graph(self,usecase: str):
         """
         Sets up the graph for the selected usecase.
@@ -29,5 +56,8 @@ class GraphBuilder:
 
         if usecase=="Basic Chatbot":
             self.basic_chatbot_build_graph()
+        
+        if usecase=="Chatbot With Web":
+            self.chatbot_with_tools_build_graph()
 
         return self.graph_builder.compile()
